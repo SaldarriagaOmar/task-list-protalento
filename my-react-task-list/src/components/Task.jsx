bb03f1387848b35c7cea3194deb116284bcdc52d
Ómar David Saldarriaga Montiel <115890675+SaldarriagaOmar@users.noreply.github.com>
2023-08-10 17:40:38 -0500

    LocalStorage listo

--- my-react-task-list/src/components/Task.jsx
+++ my-react-task-list/src/components/Task.jsx
@@ -1,41 +1,81 @@
-import "./task.css"
+import React, { useState, useEffect } from 'react';
+import './task.css';
+
 export default function Task(props) {
-    return (
-        <div className="anyTask">
-            <input type="checkbox"/>
-            <div className="nombreTarea">
-            <p>{props.description}</p>
-            </div>
-            <div className="actionbutton">
-                <button id="edit"><svg
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                >
-                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
-                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
-                </svg></button>
-                <button id="delete"><svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                >
-                    <line x1="18" y1="6" x2="6" y2="18" />
-                    <line x1="6" y1="6" x2="18" y2="18" />
-                </svg>
-                </button>
-            </div>
-        </div>)
+  const [isDone, setIsDone] = useState(true);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedDescription, setEditedDescription] = useState(props.description);
+
+  useEffect(() => {
+    const data = localStorage.getItem('toDoItems');
+    if (data) {
+      props.setTasks(JSON.parse(data));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('toDoItems', JSON.stringify(props.tasks));
+  }, [props.tasks]);
+
+  useEffect(() => {
+    localStorage.setItem('toDoItems', JSON.stringify(props.tasks));
+  }, [props.tasks]);
+
+  function handleChange(e) {
+    setEditedDescription(e.target.value);
+  }
+
+  function handleEdit() {
+    setIsEditing(true);
+  }
+
+  function handleSave() {
+    setIsEditing(false);
+  }
+
+  function confirmDelete() {
+    const confirmDelete = window.confirm(
+      'Are you sure you want to delete this task? This action cannot be undone.'
+    );
+    if (confirmDelete) {
+      props.onDelete(props.id);
+    }
+  }
+
+  return (
+    <div className="anyTask">
+      <input
+        type="checkbox"
+        checked={!isDone}
+        onChange={() => setIsDone(!isDone)}
+      />
+      <div className="nombreTarea">
+        {isEditing ? (
+          <input
+            className="taskText"
+            type="text"
+            value={editedDescription}
+            onChange={handleChange}
+            required
+          />
+        ) : (
+          <p className={isDone ? '' : 'checked'}>{props.description}</p>
+        )}
+      </div>
+      <div className="actionbutton">
+        {isEditing ? (
+          <button id="save" onClick={handleSave}>
+            Save
+          </button>
+        ) : (
+          <button id="edit" onClick={handleEdit}>
+            Edit
+          </button>
+        )}
+        <button id="delete" onClick={confirmDelete}>
+          Del
+        </button>
+      </div>
+    </div>
+  );
 }
