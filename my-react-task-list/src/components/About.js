92f60a8ec283a7a79facbe10c465a4e4a70e4121
Ómar Saldarriaga <saldarriagaomar@gmail.com>
2023-09-11 01:36:19 -0500

    enrutamiento

---
+++ my-react-task-list/src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>Acerca de Nosotros</h1>
+      <p>Nuestra aplicación de Lista de Tareas te ayuda a mantener tus tareas bajo control de manera eficiente. Algunas de sus funcionalidades incluyen:</p>
+      <ul>
+        <li>Gestión sencilla de tareas.</li>
+        <li>Seguimiento del progreso con casillas de verificación.</li>
+        <li>Eliminación fácil de tareas.</li>
+        <li>Almacenamiento persistente de datos en tu navegador.</li>
+      </ul>
+      <p>Estamos comprometidos a seguir mejorando nuestra aplicación y brindarte la mejor experiencia posible.</p>
+      <p>Esta aplicación se desarrolló utilizando tecnologías modernas como React y React Router para garantizar un rendimiento y usabilidad óptimos.</p>
+    </div>
+  );
+}
+
+export default About;
